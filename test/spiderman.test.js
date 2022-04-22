6ef6bb571ce7507175d13e67fada94fe62d675b9
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-22 12:26:20 -0500

    Paso 3: Creando la prueba con la funcionalidad que necesito

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test de pureba", () => {
-  test("Caso 1 prueba", () => {
-    const resultOfSomething = 1 + 2;
-    expect(resultOfSomething).toBe(18);
+describe("Test de Spiderman Class", () => {
+  test("1 Creando un objeto de spiderman", () => {
+    const andrewGarfield = new Spiderman(
+      "Spiderman Sony",
+      31,
+      "Andrew Garfield",
+      2,
+      "Sony"
+    );
+    expect(andrewGarfield.name).toBe("Spiderman Sony");
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.movies).toBe(2);
+    expect(andrewGarfield.studio).toBe("Sony");
   });
 });
